9054f6e94dbad018ae545edb19aafeef0e17c634
Baubs <pathogenic@gmail.com>
2022-01-03 20:17:45 -0600

    test example

--- src/main/webapp/app/modules/components/search_bar/search_bar.tsx
+++ src/main/webapp/app/modules/components/search_bar/search_bar.tsx
@@ -7,6 +7,7 @@ export interface ISearchBarProps {
 export const SearchBar = (props: ISearchBarProps) => {
   return (
     <div>
+      <h3>test</h3>
       <form action={props.action}>
         <input type="text" name="q"></input>
         <input type="submit"></input>
