4cf5751f2f05a9bd09797e077f66fc50ead2f2bf
Baubs <pathogenic@gmail.com>
2022-01-14 17:45:59 -0600

    fresh Spring/Thymeleaf project

---
+++ src/main/java/org/launchcode/PostIt/PostItApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.PostIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/PostIt/PostItApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.PostIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
