4761689d7cbb031319f4969357ee9daa2a5d103f
Baubs <pathogenic@gmail.com>
2022-01-14 17:37:38 -0600

    fresh start

--- src/main/java/org/launchcode/customhomepage/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package org.launchcode.customhomepage.repository;
-
-import org.launchcode.customhomepage.domain.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Spring Data JPA repository for the {@link Authority} entity.
- */
-public interface AuthorityRepository extends JpaRepository<Authority, String> {}
--- src/main/java/org/launchcode/customhomepage/service/dto/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Data Transfer Objects.
- */
-package org.launchcode.customhomepage.service.dto;
