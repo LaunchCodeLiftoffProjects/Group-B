4fd6eae0f9708d0f726a2c40b6d86f64991c633f
Ethan Steller <steller,e@gmail.com>
2022-01-17 19:34:45 -0600

    all files added and running on my machine

--- src/main/java/org/launchcode/PostIt/models/data/UserRepository.java
+++ src/main/java/org/launchcode/PostIt/models/data/UserRepository.java
@@ -1,4 +1,10 @@
 package org.launchcode.PostIt.models.data;
 
-public class UserRepository {
+import org.launchcode.PostIt.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    //findByUserName, returnUser, input username
+    User findByUsername(String username);
+
 }
