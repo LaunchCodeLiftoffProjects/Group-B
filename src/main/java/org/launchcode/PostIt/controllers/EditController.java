60632de693329faf24c458fd5bbfc2bf535997c3
Andrew Lamer <andrewmlamer@gmail.com>
2022-01-31 11:06:29 -0600

    saves edited items to database

--- src/main/java/org/launchcode/PostIt/controllers/EditController.java
+++ src/main/java/org/launchcode/PostIt/controllers/EditController.java
@@ -1,11 +1,15 @@
 package org.launchcode.PostIt.controllers;
 
+import org.launchcode.PostIt.models.AbstractPost;
+import org.launchcode.PostIt.models.TextPost;
 import org.launchcode.PostIt.models.data.PostRepository;
+import org.launchcode.PostIt.models.dto.TextPostFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class EditController {
@@ -17,9 +21,26 @@ public class EditController {
 
     @GetMapping("/edit/{id}")
     public String editPost(@PathVariable int id, Model model){
-        Object post = postRepository.findById(id);
+        Optional<TextPost> result = postRepository.findById(id);
+        TextPost post = result.get();
         model.addAttribute("post", post);
         return "edit";
     }
 
+    @PostMapping("edit/{id}")
+    public String savePost(@PathVariable int id, Model model, @RequestParam String newTitle, @RequestParam String newBody, @RequestParam(required = false) boolean newAnonymousFalse, @RequestParam(required = false) boolean newAnonymousTrue) {
+        Optional<TextPost> result = postRepository.findById(id);
+        TextPost post = result.get();
+        post.setBody(newBody);
+        post.setTitle(newTitle);
+        if (!post.getAnonymous()) {
+            post.setAnonymous(newAnonymousFalse);
+        } else if (post.getAnonymous()) {
+            post.setAnonymous(newAnonymousTrue);
+        }
+
+        postRepository.save(post);
+        model.addAttribute("post", post);
+        return "edit";
+    }
 }
