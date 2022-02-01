0374edc7b9dd0ca819e58b55f62942e1b4a9888d
Andrew Lamer <andrewmlamer@gmail.com>
2022-01-31 20:04:43 -0600

    added user authentication to edit post feature

--- src/main/java/org/launchcode/PostIt/controllers/EditController.java
+++ src/main/java/org/launchcode/PostIt/controllers/EditController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Controller
@@ -28,19 +29,24 @@ public class EditController {
     }
 
     @PostMapping("edit/{id}")
-    public String savePost(@PathVariable int id, Model model, @RequestParam String newTitle, @RequestParam String newBody, @RequestParam(required = false) boolean newAnonymousFalse, @RequestParam(required = false) boolean newAnonymousTrue) {
+    public String savePost(@PathVariable int id, Model model, @RequestParam String newTitle, @RequestParam String newBody, @RequestParam(required = false) boolean newAnonymousFalse, @RequestParam(required = false) boolean newAnonymousTrue, HttpSession session) {
         Optional<TextPost> result = postRepository.findById(id);
         TextPost post = result.get();
-        post.setBody(newBody);
-        post.setTitle(newTitle);
-        if (!post.getAnonymous()) {
-            post.setAnonymous(newAnonymousFalse);
-        } else if (post.getAnonymous()) {
-            post.setAnonymous(newAnonymousTrue);
+        System.out.println(authenticationController.getUserFromSession(session));
+        if(post.getUser() == authenticationController.getUserFromSession(session)) {
+            post.setBody(newBody);
+            post.setTitle(newTitle);
+            if (!post.getAnonymous()) {
+                post.setAnonymous(newAnonymousFalse);
+            } else if (post.getAnonymous()) {
+                post.setAnonymous(newAnonymousTrue);
+            }
+            postRepository.save(post);
+            model.addAttribute("post", post);
+        } else if(post.getUser() != authenticationController.getUserFromSession(session)) {
+            model.addAttribute("post", post);
+            model.addAttribute("errorMessage", "This is not your post!");
         }
-
-        postRepository.save(post);
-        model.addAttribute("post", post);
         return "edit";
     }
 }
--- src/main/java/org/launchcode/PostIt/controllers/SearchController.java
+++ src/main/java/org/launchcode/PostIt/controllers/SearchController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
