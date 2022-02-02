00fcb59c7fe964edd3d388dd92edf78b5284f54a
Baubs <pathogenic@gmail.com>
2022-02-02 17:23:42 -0600

    fixed merge conflicts

--- src/main/java/org/launchcode/PostIt/controllers/PostController.java
+++ src/main/java/org/launchcode/PostIt/controllers/PostController.java
@@ -12,15 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.Date;
+import java.util.Optional;
 
 @Controller
 public class PostController {
@@ -90,5 +88,27 @@ public class PostController {
             imagePostRepository.save(newImagePost);
             return "redirect:";
         }
+    }
+    @GetMapping("/post/{postId}")
+    public String postPage(@PathVariable int postId, Model model, HttpSession session){
+        Optional<TextPost> textPost = postRepository.findById(postId);
+        Optional<ImagePost> imagePost = imagePostRepository.findById(postId);
+        if(textPost.isPresent()){
+            model.addAttribute("post", textPost.get());
+            model.addAttribute("editButtons", false);
+            if(textPost.get().getUser() == authenticationController.getUserFromSession(session)){
+                model.addAttribute("editButtons", true);
+            }
+        } else if (imagePost.isPresent()){
+            model.addAttribute("post", imagePost.get());
+            model.addAttribute("editButtons", false);
+            if(imagePost.get().getUser() == authenticationController.getUserFromSession(session)){
+                model.addAttribute("editButtons", true);
+            }
+        } else{
+            model.addAttribute("error", "No post found");
+            model.addAttribute("editButtons", false);
+        }
+        return "postView";
     }
 }
