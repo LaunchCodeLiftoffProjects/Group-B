c2152dbfa17659a5067340f48c4121a00550130c
Baubs <pathogenic@gmail.com>
2022-02-01 18:43:50 -0600

    saving work

--- src/main/java/org/launchcode/PostIt/controllers/PostController.java
+++ src/main/java/org/launchcode/PostIt/controllers/PostController.java
@@ -9,14 +9,13 @@ import org.launchcode.PostIt.models.dto.ImagePostDTO;
 import org.launchcode.PostIt.models.dto.TextPostFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.Optional;
 
 @Controller
 public class PostController {
@@ -62,5 +61,25 @@ public class PostController {
         newImagePost.setUser(authenticationController.getUserFromSession(session));
         imagePostRepository.save(newImagePost);
         return "redirect:";
+    }
+
+    @GetMapping("/post/{postId}")
+    public String postPage(@PathVariable int postId, Model model, HttpSession session){
+        Optional<TextPost> textPost = postRepository.findById(postId);
+        Optional<ImagePost> imagePost = imagePostRepository.findById(postId);
+        if(textPost.isPresent()){
+            model.addAttribute("post", textPost.get());
+            if(imagePost.get().getUser() == authenticationController.getUserFromSession(session)){
+                model.addAttribute("editButtons", true);
+            }
+        } else if (imagePost.isPresent()){
+            model.addAttribute("post", imagePost.get());
+            if(imagePost.get().getUser() == authenticationController.getUserFromSession(session)){
+                model.addAttribute("editButtons", true);
+            }
+        } else{
+            model.addAttribute("error", "No post found");
+        }
+        return "postView";
     }
 }
